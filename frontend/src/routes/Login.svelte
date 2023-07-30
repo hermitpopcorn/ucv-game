527363ec28029b38614cd2cd0da2417f68f307f5
Dani Nugraha <dani.hmng@gmail.com>
2023-07-30 10:50:25 +0700

    updated Login subpage layout

--- frontend/src/routes/Login.svelte
+++ frontend/src/routes/Login.svelte
@@ -33,41 +33,44 @@
 	}
 </script>
 
-<section>
-	<div class="mb-8 flex flex-1 justify-center">
-		<h1 class="text-9xl text-left">UCV</h1>
-		<h2 class="text-xl self-center text-right">Unique Constraint Violation</h2>
-	</div>
+<section class="flex flex-col items-center justify-center">
+	<div style="flex: 0 0 25%" />
+	<div style="flex: 1 0 75%">
+		<article class="mb-8 flex flex-1 justify-center">
+			<h1 class="text-9xl text-left">UCV</h1>
+			<h2 class="text-xl self-center text-right">Unique Constraint Violation</h2>
+		</article>
 
-	<div class="flex flex-col items-center">
-		{#if phase == 'connect'}
-			<div class="h-12">
-				{#if $websocketConnection.state === 'connected'}
-					<div out:fade={{ duration: 90 }} in:fade={{ delay: 100 }}>
-						<Button on:click={play}>Play</Button>
-					</div>
-				{:else if $websocketConnection.state === 'connecting'}
-					<div out:fade={{ duration: 90 }} in:fade={{ delay: 100 }}>
-						<Spinner size={12} />
-					</div>
-				{:else if $websocketConnection.state === 'error' || $websocketConnection.state === 'disconnected'}
-					<div out:fade={{ duration: 90 }} in:fade={{ delay: 100 }}>
-						<Button on:click={retryConnect}>Connect</Button>
-					</div>
-				{/if}
-			</div>
-		{/if}
-		{#if phase == 'login'}
-			<div class="flex flex-col" out:fade={{ duration: 90 }} in:fade={{ delay: 100 }}>
-				<TextInput bind:value={playerName} id="player-name" label="Player Name" />
-				<Button on:click={login}>
-					{#if !loggingIn}
-						Start
-					{:else}
-						<Spinner color="red" size={6} />
+		<article class="flex flex-col items-center">
+			{#if phase == 'connect'}
+				<div class="h-12">
+					{#if $websocketConnection.state === 'connected'}
+						<div out:fade={{ duration: 90 }} in:fade={{ delay: 100 }}>
+							<Button on:click={play}>Play</Button>
+						</div>
+					{:else if $websocketConnection.state === 'connecting'}
+						<div out:fade={{ duration: 90 }} in:fade={{ delay: 100 }}>
+							<Spinner size={12} />
+						</div>
+					{:else if $websocketConnection.state === 'error' || $websocketConnection.state === 'disconnected'}
+						<div out:fade={{ duration: 90 }} in:fade={{ delay: 100 }}>
+							<Button on:click={retryConnect}>Connect</Button>
+						</div>
 					{/if}
-				</Button>
-			</div>
-		{/if}
+				</div>
+			{/if}
+			{#if phase == 'login'}
+				<div class="flex flex-col" out:fade={{ duration: 90 }} in:fade={{ delay: 100 }}>
+					<TextInput bind:value={playerName} id="player-name" label="Player Name" />
+					<Button on:click={login}>
+						{#if !loggingIn}
+							Start
+						{:else}
+							<Spinner color="red" size={6} />
+						{/if}
+					</Button>
+				</div>
+			{/if}
+		</article>
 	</div>
 </section>
