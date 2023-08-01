ae49a0d0236dcc26427e0495a9bb14b7441f202d
Dani Nugraha <dani.hmng@gmail.com>
2023-08-01 11:45:21 +0700

    changed Button component to be able to take extra classes

--- frontend/src/lib/Button.svelte
+++ frontend/src/lib/Button.svelte
@@ -1,9 +1,14 @@
-<button
-	class="bg-blue-500 hover:bg-blue-400 text-white
-	font-bold py-2 px-4 border-b-4 border-blue-700
-	hover:border-blue-500 rounded"
-	on:click
->
+<script lang="ts">
+	let extraClasses = '';
+	export { extraClasses as class };
+
+	let defaultClasses =
+		'bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded';
+
+	let classes = [defaultClasses, extraClasses].join(' ');
+</script>
+
+<button class={classes} on:click>
 	<div class="flex items-center justify-center">
 		<slot />
 	</div>
